2ed52295e79fc081d5c08bd15b1bb39cf9eb070b
Aziel-Zhero <aziel_@outlook.com.br>
2025-01-13 21:05:45 -0300

    novos recursos

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
   function loadTableData() {     
       setTimeout(() => {        
           const data = [
-              { nome: "João Silva", telefone: "(11) 99999-9999", ramal: "1234", setor: "TI" },
-              { nome: "Maria Souza", telefone: "(11) 98888-8888", ramal: "5678", setor: "RH" },
-              { nome: "Carlos Oliveira", telefone: "(11) 97777-7777", ramal: "1122", setor: "Financeiro" },
-              { nome: "Ana Lima", telefone: "(11) 96666-6666", ramal: "3344", setor: "Vendas" },
-              { nome: "Ana Lima", telefone: "(11) 96666-6666", ramal: "3344", setor: "Vendas" },
-              { nome: "Ana Lima", telefone: "(11) 96666-6666", ramal: "3344", setor: "Vendas" },
-              { nome: "Ana Lima", telefone: "(11) 96666-6666", ramal: "3344", setor: "Vendas" },
+              { nome: "Nome 1", telefone: "(11) 99999-9999", ramal: "1234", setor: "TI" },
+              { nome: "Nome 2", telefone: "(11) 98888-8888", ramal: "5678", setor: "RH" },
+              { nome: "Nome 3", telefone: "(11) 97777-7777", ramal: "1122", setor: "Financeiro" },
+              { nome: "Nome 4", telefone: "(11) 96666-6666", ramal: "3344", setor: "Vendas" },
+              { nome: "Nome 5", telefone: "(11) 96666-6666", ramal: "3344", setor: "Vendas" },
+              { nome: "Nome 6", telefone: "(11) 96666-6666", ramal: "3344", setor: "Vendas" },
+              { nome: "Nome 7", telefone: "(11) 96666-6666", ramal: "3344", setor: "Vendas" },
           ];
 
           document.getElementById('table-container').style.display = 'none';
@@ -29,3 +29,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
   loadTableData();
 });
+
+function toggleSubmenu(event) {
+    event.preventDefault(); // Previne a navegação do link
+
+    var submenu = event.target.nextElementSibling;
+    var isOpen = submenu.classList.contains("open");
+
+    // Animar com GSAP
+    if (isOpen) {
+        gsap.to(submenu, { 
+            duration: 0.5, 
+            maxHeight: 0, 
+            ease: "power2.out"
+        });
+        submenu.classList.remove("open");
+    } else {
+        submenu.classList.add("open");
+        gsap.to(submenu, { 
+            duration: 0.5, 
+            maxHeight: 500, 
+            ease: "power2.out" 
+        });
+    }
+}
